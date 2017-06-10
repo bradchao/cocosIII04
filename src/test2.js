9913028d7e7f4a80ba131dbeef0915ddb1b9effa
jack000159 <jack000159@gmail.com>
2017-06-10 13:42:32 +0800

    20170610v5

--- src/test2.js
+++ src/test2.js
@@ -22,12 +22,44 @@ var Test2Layer = cc.Layer.extend({
         if (this.ball.x + this.ball.width/2 >= cc.winSize.width ||
             this.ball.x - this.ball.width/2 <= 0){
             this.dx *= -1;
+
+            if (this.dx * this.dy < 0){
+                this.ball.runAction(cc.rotateBy(0.5, 90));
+            }else{
+                this.ball.runAction(cc.rotateBy(0.5, -90));
+            }
+
+
+            // if (this.dx > 0){
+            //     // - => +
+            //     if (this.dy>0){
+            //         // ang => 90
+            //         this.ball.runAction(cc.rotateBy(0.5, -90));
+            //     }else{
+            //         // ang = -90
+            //         this.ball.runAction(cc.rotateBy(0.5, 90));
+            //     }
+            // }else{
+            //     // + => -
+            //     if (this.dy >0){
+            //         // ang => -90
+            //         this.ball.runAction(cc.rotateBy(0.5, 90));
+            //     }else{
+            //         // ang => 90
+            //         this.ball.runAction(cc.rotateBy(0.5, -90));
+            //     }
+            // }
+
+
+
         }
         if (this.ball.y + this.ball.height/2 >= cc.winSize.height ||
             this.ball.y - this.ball.height/2 <= 0){
             this.dy *= -1;
+
+            this.ball.runAction(cc.rotateBy(0.5, 90));
         }
-        
+
         this.ball.x += this.dx;
         this.ball.y += this.dy;
     },
