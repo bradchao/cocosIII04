ccfc389299022704965428bfa7a88bd9a0400cba
jack000159 <jack000159@gmail.com>
2017-06-10 12:01:37 +0800

    20170610v4

--- src/test2.js
+++ src/test2.js
@@ -1,6 +1,8 @@
 
 var Test2Layer = cc.Layer.extend({
     ball:null,
+    dx : 4,
+    dy : 4,
     ctor:function () {
         this._super();
         var size = cc.winSize;
@@ -10,12 +12,25 @@ var Test2Layer = cc.Layer.extend({
             x: size.width / 2,
             y: size.height / 2
         });
-        this.ball.scaleX = 2;
-        this.ball.scaleY = 2;
         this.addChild(this.ball);
 
+        this.scheduleUpdate();
+
         return true;
-    }
+    },
+    update: function (dt) {
+        if (this.ball.x + this.ball.width/2 >= cc.winSize.width ||
+            this.ball.x - this.ball.width/2 <= 0){
+            this.dx *= -1;
+        }
+        if (this.ball.y + this.ball.height/2 >= cc.winSize.height ||
+            this.ball.y - this.ball.height/2 <= 0){
+            this.dy *= -1;
+        }
+        
+        this.ball.x += this.dx;
+        this.ball.y += this.dy;
+    },
 });
 
 var Test2Scene = cc.Scene.extend({
