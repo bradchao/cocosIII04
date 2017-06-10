f4056ebf42e902541d59c0ad81bc553267650931
jack000159 <jack000159@gmail.com>
2017-06-10 14:48:30 +0800

    20170610v7

--- src/resource.js
+++ src/resource.js
@@ -3,7 +3,10 @@ var res = {
     bullet_png : "res/bullet.png",
     b2_png : "res/b2.png",
     brick_png : "res/brick.png",
+    bg1_png : "res/bg1.png",
+    man_png : "res/man.png",
     ball_png : "res/ball.png",
+    man_plist : "res/man.plist",
 };
 
 var g_resources = [];
--- src/test3.js
+++ src/test3.js
@@ -1,19 +1,67 @@
 
-var TempLayer = cc.Layer.extend({
-    sprite:null,
+var Test3Layer = cc.Layer.extend({
+    bg:null,
+    isRight: true,
     ctor:function () {
         this._super();
         var size = cc.winSize;
 
+        this.bg = new cc.Sprite(res.bg1_png);
+        this.bg.attr({
+            x : this.bg.width / 2,
+            y : size.height / 2
+        });
+        this.bg.scaleY = 2;
+        this.addChild(this.bg);
+
+        this.myKeyListener(this);
 
         return true;
+    },
+
+    myKeyListener : function (layer) {
+      cc.eventManager.addListener({
+          event: cc.EventListener.KEYBOARD,
+          onKeyPressed: function (keyCode, event) {
+              switch (keyCode){
+                  case 39:
+                      // 往右
+                      if (!layer.isRight){
+                          // man change dir
+                      }
+                      layer.goForwardBg();
+                      break;
+                  case 37:
+                      if (layer.isRight){
+                          // man change dir
+                      }
+                      layer.goBackBg();
+                      break;
+              }
+          },
+      }, this);
+    },
+
+    goForwardBg: function () {
+        if (this.bg.x + this.bg.width/2 > cc.winSize.width){
+            this.bg.x -= 8
+        }
+    },
+
+    goBackBg: function () {
+        if (this.bg.x - this.bg.width/2 < 0){
+            this.bg.x += 8
+        }
     }
+
+
+
 });
 
-var TempScene = cc.Scene.extend({
+var Test3Scene = cc.Scene.extend({
     onEnter:function () {
         this._super();
-        var layer = new TempLayer();
+        var layer = new Test3Layer();
         this.addChild(layer);
     }
 });
